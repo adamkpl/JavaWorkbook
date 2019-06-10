65c6a12642b4e873d7da0e401cd23c9425ab1e99
adamkpl <adamswro@gmail.com>
2019-06-10 18:48:52 +0200

    First published version.

---
+++ src/main/java/application.java
@@ -0,0 +1,9 @@
+public class application {
+    public static void main(String[] args){
+        String humanName = "Peter";
+        String humanSurname = "Parker";
+        System.out.println("Hi," + " " + humanName + " " + humanSurname + ".");
+/*        System.out.print("Hello World");*/
+
+    }
+}
