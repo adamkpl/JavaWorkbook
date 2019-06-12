e7c176530c47f0fd4517236cfab108a44c58e549
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-06-12 18:49:39 +0200

    lesson: Arrays

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,7 +1,18 @@
 public class application {
-    public static void main(String[] args){
-        String humanName = "Peter";
-        String humanSurname = "Parker";
-        System.out.println("Hi," + " " + humanName + " " + humanSurname + ".");
+    public static void main(String[] args) {
+
+        //lesson: Arrays, part 2/4
+
+        // define variables
+        int [ ] myArray = {6, 42, 3, 7}; // 4 elements, last one index=3
+        int sum=0;
+
+        // open FOR loop & define new variable x
+        for(int index=0; index<myArray.length; index++) {
+            sum = sum + myArray[index];
+        }
+
+        // print result
+        System.out.println(sum); // = 58
     }
 }
