ca5c048f78e71fdf05a1aa744be602d0f74b9bec
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-07-08 19:51:33 +0200

    08.07.2019

---
+++ src/main/java/java/core/adam/Vehicle.java
@@ -0,0 +1,24 @@
+/* SoloLearn, lesson: Classes and Objects: Getters and setters, part 7/13 */
+
+public class Vehicle {
+    private String color;               // private = Accessible only within the declared class itself.
+
+    // Getter
+    public String getColor() {
+        return color;
+    }
+
+    // Setter
+    public void setColor(String c) {    // Does not return any value.
+        this.color = c;                 // The keyword this is used to refer to the current object. Basically, this.color is the color attribute of the current object. Use "this" to differentiate instance variable from local variable. More: https://pl.wikipedia.org/wiki/This_(programowanie_obiektowe)
+    }
+}
+
+class pimpMyRide {
+
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        v1.setColor("Iron Man's Sexy Red");
+        System.out.println("New color: " + v1.getColor());
+    }
+}
