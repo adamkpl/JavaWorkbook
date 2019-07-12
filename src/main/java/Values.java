25a8b60313bdb3f8caacd0246b7a5b9ddd8c6c4e
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-07-12 18:26:03 +0200

    12.07.2019

---
+++ src/main/java/Values.java
@@ -0,0 +1,14 @@
+/* SoloLearn, lesson: Classes and Objects: Value & Reference Types, part 9/13 */
+
+public class Values {
+    public static void main(String[ ] args) { // This where the main executes everything.
+        int x = 0; // sample variable and its value
+        addOneTo(x); // this method is invoked from the code below, and inserts a new argument (int x=5).
+        System.out.println("main method = " + x); // Outputs "5"
+    }
+
+    static void addOneTo(int num) { // method called addOneTo with a int-type 'num' variable.
+        num = num + 1; // increment by 1
+        System.out.println("addOneTo method = " + num); // Outputs "6". See https://www.youtube.com/watch?v=eRfvgSvf-mM.
+        /*  x is defined first, and also in public static void main(String[ ] args), not in the addOneTo() method. When the method is called, it creates a new variable = num, defined in the method itself. The value of x is "passed" to num, but they're different variables, defined in different spaces. The method only uses num, not x, and modifies only num, not x. */    }
+}
