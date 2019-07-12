267fabb8683cfbe07f05b5b3373df83597b8fcf5
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-07-12 18:35:47 +0200

    12.07.2019

--- src/main/java/Values.java
+++ src/main/java/Values.java
@@ -9,6 +9,6 @@ public class Values {
 
     static void addOneTo(int num) { // method called addOneTo with a int-type 'num' variable.
         num = num + 1; // increment by 1
-        System.out.println("addOneTo method = " + num); // Outputs "1". See https://www.youtube.com/watch?v=eRfvgSvf-mM. x is defined first, and also in public static void main(String[ ] args), not in the addOneTo() method. When the method is called, it creates a new variable = num, defined in the method itself. The value of x is "passed" to num, but they're different variables, defined in different spaces. The method only uses num, not x, and modifies only num, not x.
+        System.out.println("addOneTo method = " + num); // Outputs "1". See https://www.youtube.com/watch?v=eRfvgSvf-mM. x is defined first, and also in public static void main(String[ ] args), not in the addOneTo() method. When the method is called, it creates a new variable = num, defined in the method itself. The value of x is "passed" to num, but they're different variables, defined in different spaces (that's the most important information!). The method only uses num, not x, and modifies only num, not x.
     }
 }
