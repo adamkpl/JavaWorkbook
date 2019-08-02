895ef5e5e3d332de569127e5aaf5a7da9c10d333
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-08-02 15:24:54 +0200

    02.08.2019

--- src/main/java/com/horstmann/InputOutput.java
+++ src/main/java/com/horstmann/InputOutput.java
@@ -3,13 +3,13 @@
 
 /**
  * Reading & Writing contents into files
- * @version 1.0 2019-08-01
+ * @version 1.1 2019-08-02
  * @author Adam K.
  */
 
-/*1. Ask user for an input.
- * 2. Save it into a .txt file.
- * 3. Read it.*/
+/* 1. Ask user for an input until a single character is given.
+ * 2. Save output into a .txt file.
+ * 3. Read it. */
 
 package com.horstmann;
 
@@ -22,14 +22,22 @@ public class InputOutput {
     public static void main(String[] args) throws IOException {
 
         //Ask user for input
-        System.out.println("Hi! What is your name?");
         Scanner readInput = new Scanner(System.in);
+        System.out.println("Type anything: ");
         String userInput = readInput.nextLine();
 
-        if(userInput.isEmpty()) {
-            System.out.println("No input.");
-        } else {
-            System.out.println("User's input: " + userInput + ". " + "Saving it into a file.");
+        //3.8.3. Pętle
+        do
+        {
+            if (userInput.length() == 0)
+            {
+                System.out.println("No input. Try again. " + "User input length: " + userInput.length());
+                userInput = readInput.nextLine();
+            }
+        }
+        while (userInput.length() == 0);
+        {
+            // no instructions here as intended.
         }
 
         // 3.7.3. Zapis i odczyt plików
@@ -40,17 +48,26 @@ public class InputOutput {
         saveOutput.print(userOutput);
         saveOutput.close();
 
+        System.out.println("User's input: " + userInput + ". ");
+        System.out.println("User input length: " + userInput.length());
+        System.out.println("Saving results into an output file.");
+
         //Read contents from a local file
         //Scanner localTextfile = new Scanner(Paths.get("..\\out.txt"));
         Scanner localTextfile = new Scanner(Paths.get("src\\main\\java\\com\\horstmann\\out.txt"));
 
-        if (localTextfile.hasNext()) {
-            while (localTextfile.hasNext()) {
-                System.out.println("File's content: " + localTextfile.nextLine());
+        if (localTextfile.hasNext())
+        {
+            while (localTextfile.hasNext())
+            {
+                System.out.println("Output file content: " + localTextfile.nextLine());
             }
-        } else {
-            System.out.println("No content to display.");
         }
+        else
+        {
+            System.out.println("No content to display!");
+        }
+
     }
 
 }
