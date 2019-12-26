41749a0b5df580ab0c4595a898bb7bf30cfc0db1
adamkpl <50586694+adamkpl@users.noreply.github.com>
2019-12-26 16:57:02 +0100

    26.12.2019
    Minor correction in Java for Testers classes and test.

--- src/main/java/com/javafortesters/domainobject/WebAddress.java
+++ src/main/java/com/javafortesters/domainobject/WebAddress.java
@@ -2,19 +2,8 @@ package com.javafortesters.domainobject;
 
 public class WebAddress {
 
-    public static final String url = "https://javafortesters.com/";
+    public static final String URL = "https://javafortesters.com/";
     public static final String IP = "185.119.173.30";
-    public static final String PORT = "80";
+    public static final int PORT = 80;
 
-    public static String getUrl() {
-        return url;
-    }
-
-    public static String getIp() {
-        return IP;
-    }
-
-    public static String getPort() {
-        return PORT;
-    }
 }
--- src/test/java/com/javafortesters/WebAddressTest.java
+++ src/test/java/com/javafortesters/WebAddressTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertEquals;
 public class WebAddressTest {
     @Test
     public void canGetWebUrl() {
-        assertEquals("Returns URL","https://javafortesters.com/", WebAddress.getUrl());
+        assertEquals("Returns URL","https://javafortesters.com/", WebAddress.URL);
     }
 
     @Test
     public void canGetWebIpAndPort() {
-        assertEquals("Returns IP","185.119.173.30", WebAddress.getIp());
-        assertEquals("Returns PORT","80", WebAddress.getPort());
+        assertEquals("Returns IP","185.119.173.30", WebAddress.IP);
+        assertEquals("Returns PORT",80, WebAddress.PORT);
     }
 }
