39c008472c97de6ddee4d0353402e3f68af1362e
adamkpl <50586694+adamkpl@users.noreply.github.com>
2020-02-07 14:47:40 +0100

    07.02.2020. Implemented takeScreenshot static method for taking screenshots of web pages. Upgraded the webdrivermanager version to 3.8.1. Added some basic Allure reporting for com.javafortesters tests.

--- src/main/java/com/javafortesters/domainobject/WebAddress.java
+++ src/main/java/com/javafortesters/domainobject/WebAddress.java
@@ -1,9 +1,31 @@
 package com.javafortesters.domainobject;
 
+import java.net.Inet4Address;
+import java.net.URL;
+
 public class WebAddress {
 
-    public static final String URL = "https://javafortesters.com/";
-    public static final String IP = "185.119.173.30";
-    public static final int PORT = 80;
+    private URL url;
+    private Inet4Address ip;
+
+    public static final String javaForTesters = "https://www.javafortesters.com/";
+    public static final String javaForTestersBook = "https://www.javafortesters.com/page/about/";
+    public static final String javaForTestersIp = "185.119.173.30";
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public Inet4Address getIp() {
+        return ip;
+    }
+
+    public void setIp(Inet4Address ip) {
+        this.ip = ip;
+    }
 
 }
