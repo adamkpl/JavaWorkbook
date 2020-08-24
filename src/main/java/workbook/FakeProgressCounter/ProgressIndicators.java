8029e84c6673d93729846577916b867e1130c8a1
adamkpl <50586694+adamkpl@users.noreply.github.com>
2020-08-24 17:52:36 +0200

    24.08.2020. Update Progress Indicators & Process Names

--- src/main/java/workbook/FakeProgressCounter/ProgressIndicators.java
+++ src/main/java/workbook/FakeProgressCounter/ProgressIndicators.java
@@ -1,7 +1,11 @@
 package workbook.FakeProgressCounter;
 
 public class ProgressIndicators {
+    public final static String FILES = "File(s)";
+    public final static String INFORMATION = "Information";
     public final static String PERCENT = "%";
+    public final static String RESOURCE = "Resource(s)";
+    public final static String SOURCE = "Source(s)";
     public final static String STEP = "Step(s)";
     public final static String TASK = "Task(s)";
     public final static String THREAD = "Thread(s)";
--- src/main/java/workbook/FakeProgressCounter/ProgressProcessNames.java
+++ src/main/java/workbook/FakeProgressCounter/ProgressProcessNames.java
@@ -1,15 +1,27 @@
 package workbook.FakeProgressCounter;
 
 public class ProgressProcessNames {
-    public final static String STARTING = "Starting";
+    public final static String ANALYZING = "Analyzing";
+    public final static String BUILDING = "Building";
+    public final static String CHECKING = "Checking";
+    public final static String COMPILING = "Compiling";
+    public final static String COMPLETING = "Completing";
+    public final static String COPYING = "Copying";
+    public final static String FINISHING = "Finishing";
+    public final static String EXECUTING = "Executing";
     public final static String LOADING = "Loading";
+    public final static String PARSING = "Parsing";
     public final static String PROCESSING = "Processing";
     public final static String PROGRESSING = "Progressing";
+    public final static String REPLICATING = "Replicating";
+    public final static String RUNNING = "Running";
+    public final static String STARTING = "Starting";
+    public final static String SYNCHRONIZING = "Synchronizing";
+    public final static String UPDATING = "Updating";
     public final static String VERIFYING = "Verifying";
     public final static String VALIDATING = "Validating";
-    public final static String BUILDING = "Building";
-    public final static String FINISHING = "Finishing";
-    public final static String COMPLETING = "Completing";
+    public final static String WRITING = "Writing";
+    // todo Completion
     public final static String DONE = "DONE";
     public final static String COMPLETED = "COMPLETED";
     public final static String FINISHED = "FINISHED";
