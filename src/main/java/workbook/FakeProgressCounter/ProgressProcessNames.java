6ffa18a3db5704e02b588a2d108336804040afc8
Adam Kaczmarek <adam.kaczmarek@atos.net>
2020-08-24 13:39:01 +0200

    24.08.2020 Update FakeProgressCounter
    Fix a typo

--- src/main/java/workbook/FakeProgressCounter/ProgressProcessNames.java
+++ src/main/java/workbook/FakeProgressCounter/ProgressProcessNames.java
@@ -7,7 +7,7 @@ public class ProgressProcessNames {
     public final static String PROGRESSING = "Progressing";
     public final static String VERIFYING = "Verifying";
     public final static String VALIDATING = "Validating";
-    public final static String BUILDING = "Buliding";
+    public final static String BUILDING = "Building";
     public final static String FINISHING = "Finishing";
     public final static String COMPLETING = "Completing";
     public final static String DONE = "DONE";
