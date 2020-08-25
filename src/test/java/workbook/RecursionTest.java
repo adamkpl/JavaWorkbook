6c66641b4e415d958e8ac7ae0136b81a21c35fbb
adamkpl <50586694+adamkpl@users.noreply.github.com>
2020-08-25 17:04:16 +0200

    25.08.2020. Add RecursionTest
    Work in progress. JUnit throws java.lang.IllegalArgumentException

---
+++ src/test/java/workbook/RecursionTest.java
@@ -0,0 +1,44 @@
+package workbook;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(value = Parameterized.class)
+public class RecursionTest {
+
+    // Parameters
+    private long testedValue;
+    private long expectedValue;
+
+    // Constructor is initialized with one set of parameters every time
+    public RecursionTest(long expectedValue, long testedValue) {
+        this.expectedValue = expectedValue;
+        this.testedValue = testedValue;
+    }
+
+    // @Parameters annotation marks this method as parameters provider
+    // todo throws java.lang.IllegalArgumentException: wrong number of arguments???
+    @Parameterized.Parameters
+    public static Iterable<Object[]> testData() {
+        return Arrays.asList(new Object[][] {
+                {3},{6}
+                /*{-1},{1}
+                ,{0},{1}
+                ,{1},{1}*/
+                //todo add more values
+        });
+    }
+
+
+    @Ignore
+    @Test
+    public void shouldOutputValidResultsWithARecursiveMethod() {
+        assertEquals(expectedValue, Recursion.calculateFactorial(testedValue));
+    }
+}
