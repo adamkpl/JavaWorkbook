e64016c1405fe4f7cf351b35f575723ab1156e56
adamkpl <50586694+adamkpl@users.noreply.github.com>
2020-12-17 12:48:57 +0100

    Minor cleanup for jsonplaceholder package files

--- src/test/java/com/typicode/jsonplaceholder/comments/GetCommentTest.java
+++ src/test/java/com/typicode/jsonplaceholder/comments/GetCommentTest.java
@@ -1,24 +1,17 @@
 package com.typicode.jsonplaceholder.comments;
 
 import com.typicode.jsonplaceholder.utils.Endpoints;
-import com.typicode.jsonplaceholder.utils.StatusCodes;
+import com.utils.StatusCode;
+import io.qameta.allure.Description;
 import io.qameta.allure.Link;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import io.qameta.allure.Description;
-import io.qameta.allure.junit4.DisplayName;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
-/**
- * Get Comments Test
- * @version     alpha 0.2
- * @since       2020-02-07
- * @author      Adam K.
- */
+import static org.hamcrest.Matchers.hasItem;
 
 @Link("https://jsonplaceholder.typicode.com/comments")
 @Link(name = "JSONPlaceholder", type = "Fake Online REST API for Testing and Prototyping")
@@ -28,31 +21,30 @@ public class GetCommentTest {
     private static Response response;
 
     @BeforeClass
-    public static void getCommentsEndpoint(){
-        response = given().expect().statusCode(StatusCodes.SC_OK).
-                when().get(Endpoints.COMMENTS_ENDPOINT);
+    public static void getCommentsEndpoint() {
+        response = given()
+                    .expect()
+                    .statusCode(StatusCode.SC_OK)
+                .when()
+                    .get(Endpoints.COMMENTS);
     }
 
     @Test
     @DisplayName("Verify Content Type is JSON")
     @Description("Verify that requests content type is JSON")
-    public void verifyContentTypeJSON(){
-        response.then().assertThat().contentType(ContentType.JSON);
+    public void getCommentsAssertContentTypeIsJSON() {
+        response.then()
+                    .assertThat()
+                    .contentType(ContentType.JSON);
     }
-/*
-    @Test
-    @DisplayName("Receive response in 250ms or less")
-    @Description("Ensure that request response time is 250ms or less")
-    public void canGetResponseInLessThan250ms(){
-        response.then().assertThat().time(lessThan(2500L));
-    }
-*/
 
     @Test
     @DisplayName("Receive a specific record")
     @Description("Find and get an existing record with a specific content")
-    public void canGetSpecificRecord(){
-        response.then().assertThat().body("email", hasItem("Eliseo@gardner.biz"));
+    public void getCommentsGetSpecificRecord() {
+        response.then()
+                    .assertThat()
+                    .body("email", hasItem("Eliseo@gardner.biz"));
     }
 
 }
