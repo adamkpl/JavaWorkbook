e004f65495060f1a11f50d723dba04ef3253578f
adamkpl <50586694+adamkpl@users.noreply.github.com>
2021-04-22 21:54:52 +0200

    Add new pattern to Patterns.java

--- src/main/java/workbook/Patterns.java
+++ src/main/java/workbook/Patterns.java
@@ -3,22 +3,57 @@ package workbook;
 public class Patterns {
     public static void main(String[] args) {
 
-/*
-        Right Triangle Star Pattern
+        /*
+        Right Triangle
         *
         * *
         * * *
         * * * *
         * * * * *
-*/
-        rightTriangleStarPattern(5);
+        */
+        rightTriangle(5);
 
+        System.out.println("");
+
+        /*
+        Pyramid Program
+            *
+           * *
+          * * *
+         * * * *
+        * * * * *
+        */
+        pyramidTriangle(5);
 
     }
 
-    private static void rightTriangleStarPattern(int rows) {
-        for(int i = 1; i <= rows; ++i) {
-            for(int j = 1; j <= i; ++j) {
+    public static void pyramidTriangle(int rows) {
+        // outer loop to handle number of rows
+        // rows in this case
+        for (int i = 0; i < rows; i++) {
+
+            // inner loop to handle number spaces
+            // values changing acc. to requirement
+            for (int j = rows - i; j > 1; j--) {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+            for (int j = 0; j <= i; j++) {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
+        }
+    }
+
+    private static void rightTriangle(int rows) {
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
                 System.out.print("* ");
             }
             System.out.println();
